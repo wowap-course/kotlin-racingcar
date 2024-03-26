9ff2db063b228c4f2524633a14e3570955464000
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-03-26 23:05:51 +0900

    feat: 사용자로부터 입력받기

--- src/main/kotlin/racingcar/Controller.kt
+++ src/main/kotlin/racingcar/Controller.kt
@@ -1,5 +1,12 @@
 package racingcar
 
+import java.util.Scanner
+
 fun main() {
+    val input = Scanner(System.`in`)
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val name = input.nextLine().split(',')
+    println("시도할 횟수는 몇 회인가요?")
+    val times = input.nextInt()
 
 }
