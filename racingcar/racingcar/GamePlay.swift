a6739c6ad80db4967e2a75b3d3483da9bbfeabe1
김민석|Minseok Kim <alstjr7437@naver.com>
2024-03-27 01:58:28 +0900

    feat: 레이싱 게임 구현

--- racingcar/racingcar/GamePlay.swift
+++ racingcar/racingcar/GamePlay.swift
@@ -1,1 +1,29 @@
+//
+//  GamePlay.swift
+//  racingcar
+//
+//  Created by KimMinSeok on 3/27/24.
+//
 
+import Foundation
+
+func act(count: Int, cars: [Car]){
+    for _ in 0..<count{
+        for car in cars{
+            car.go()
+            let dashes = String(repeating: "-", count: car.position)
+            print("\(car.name) : \(dashes)")
+        }
+        print()
+    }
+}
+
+func win(cars : [Car]){
+    let maxPosition = cars.map { $0.position }.max() ?? 0
+    print("최종 우승자: ", terminator: "")
+    for car in cars{
+        if car.isWinner(winnerPosition: maxPosition) {
+            print(car.name, terminator: "")
+        }
+    }
+}
