ecf1f05bf0042cbdf3908e66331e3691a3e94a71
김민석|Minseok Kim <alstjr7437@naver.com>
2024-03-27 01:52:22 +0900

    feat: 인풋 로직 구현

--- racingcar/racingcar/main.swift
+++ racingcar/racingcar/main.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 func main(){
+    let cars = inputData()
+    
+    print("시도할 횟수는 몇회인가요?")
+    let count = Int(readLine()!)!
 
 }
 
+func inputData() -> [Car]{
+    print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    var cars: [Car] = []
+    let carInput = readLine()!.split(separator: ",").map{ String($0) }
+    
+    for name in carInput{
+        cars.append(Car(name: name))
+    }
+    
+    return cars
+}
 
 
 
