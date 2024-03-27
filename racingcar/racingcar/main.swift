879d33d7f426c8eb4bb3c47808c53e99e5344652
김민석|Minseok Kim <alstjr7437@naver.com>
2024-03-28 00:42:06 +0900

    feat: 에러처리 부분 추가

--- racingcar/racingcar/main.swift
+++ racingcar/racingcar/main.swift
@@ -4,11 +4,16 @@
 //
 //  Created by KimMinSeok on 3/26/24.
 //
+enum carInputError : Error{
+    case invalidArgumentException
+}
 
 import Foundation
 
 func main(){
-    let cars = inputData()
+    guard let cars = inputData() else {
+        return
+    }
     
     print("시도할 횟수는 몇회인가요?")
     let count = Int(readLine()!)!
@@ -17,22 +22,30 @@ func main(){
     findWinners(cars: cars)
 }
 
-func inputData() -> [Car]{
-    print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    var cars: [Car] = []
-    let carInput = readLine()!.split(separator: ",").map{ String($0) }
-    
-    for name in carInput{
-        cars.append(Car(name: name))
+func inputData() -> [Car]?{
+    do {
+        print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        var cars: [Car] = []
+        let carInput = readLine()!.split(separator: ",").map{ String($0) }
+        
+        for car in carInput{
+            if car.count > 5{
+                throw carInputError.invalidArgumentException
+            }
+        }
+        
+        for name in carInput{
+            cars.append(Car(name: name))
+        }
+        
+        return cars
+    } catch {
+        print("Error: 차 이름이 5글자가 넘었습니다.")
+        return nil
     }
-    
-    return cars
 }
 
 
 
 main()
-
-
-
 
