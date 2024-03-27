1da97372e22f55871c3f8d3e1599b52f4ca1fb77
SeongWooHong <pluto0303@naver.com>
2024-03-27 20:29:42 +0900

    feat: add fun for game operate

--- src/main/kotlin/racingcar/RacingManager.kt
+++ src/main/kotlin/racingcar/RacingManager.kt
@@ -11,6 +11,26 @@ class RacingManager {
         inputRepeatTime()
     }
 
+    fun gameRun() {
+        printResultCommand()
+        for (i in 1..repeatTime) {
+            racingOperation()
+
+        }
+    }
+
+    fun printWinner() {
+        print("최종 우승자: ")
+    }
+
+    private fun racingOperation() {
+        for (car in racingCars) {
+            if(randomNumberOverFour()) {
+                car.moveForward()
+            }
+        }
+    }
+
     private fun printInsertCarNameCommand() {
         println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
     }
