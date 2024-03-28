8212f11627031168dafa81afe8e2b3c4db67994b
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-03-28 11:40:51 +0900

    refactor: Car내부에서 이름 조건 판단, toString 제거

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,9 +1,13 @@
 package racingcar
 
-class Car(val name : String) {
+class Car(input : String) {
+    val name : String
     var position : Int = 0
+    init{
+        if(input.length > 5) throw IllegalArgumentException("Name can't be longer than 5")
+        name = input
+    }
     fun move(condition : Int) {
         if(condition > 3) position++
     }
-    override fun toString(): String = "${name}: " + "-".repeat(position)
 }
--- src/main/kotlin/racingcar/Controller.kt
+++ src/main/kotlin/racingcar/Controller.kt
@@ -7,12 +7,9 @@ fun main() {
     val input = Scanner(System.`in`)
     println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
     val name = input.nextLine().split(',')
-    name.forEach{
-        if(it.length > 5) throw IllegalArgumentException("Name can't be longer than 5")
-    }
+    val cars = name.map { Car(it) }
     println("시도할 횟수는 몇 회인가요?")
     val times = input.nextInt()
-    val cars = name.map { Car(it) }
     val random = Random()
 
     repeat(times){
