b22e37007c7f9f6459078ccc35edfcc79c4bf6d1
SeongWooHong <pluto0303@naver.com>
2024-03-28 20:37:03 +0900

    fix: 부호 반대로

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,7 +4,7 @@ class Car(val name: String, var distance: Int) {
 
     init {
 //        if(name.length > 5) throw IllegalArgumentException("")
-        require(name.length > 5){"자동차 이름의 길이는 5를 초과할 수 없습니다."}
+        require(name.length < 6){"자동차 이름의 길이는 5를 초과할 수 없습니다."}
 //        check()
     }
 
