934ef7068eebbc37457ad933d62f7133880066d4
SeongWooHong <pluto0303@naver.com>
2024-03-28 20:38:59 +0900

    refactor: 상수화

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,11 +4,15 @@ class Car(val name: String, var distance: Int) {
 
     init {
 //        if(name.length > 5) throw IllegalArgumentException("")
-        require(name.length < 6){"자동차 이름의 길이는 5를 초과할 수 없습니다."}
+        require(name.length < MAX_NAME_LENGTH){"자동차 이름의 길이는 5를 초과할 수 없습니다."}
 //        check()
     }
 
     fun moveForward() {
         distance++
+    }
+
+    companion object {
+        const val MAX_NAME_LENGTH = 5
     }
 }
