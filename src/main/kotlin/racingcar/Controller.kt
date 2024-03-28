8be35db2441e1b74f55ca03f318b748a71352505
Kim Min Je <kmz34180@naver.com>
2024-03-28 17:00:03 +0900

    feat: main & fun input name

--- src/main/kotlin/racingcar/Controller.kt
+++ src/main/kotlin/racingcar/Controller.kt
@@ -1,6 +1,17 @@
 package racingcar
 
 fun main() {
-    // TODO: 프로그램 구현
-    println("fsf")
+    val carName = inputName()
+    val count = inputCount()
+    val carPositions = Array(carName.size) { 0 }
+    raceStart(count, carName, carPositions)
+    printWinners(carName, carPositions)
+}
+
+fun inputName(): List<String> {
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val input = readlnOrNull() ?: ""
+    val carName = input.split(",")
+    validCarName(carName)
+    return carName
 }
