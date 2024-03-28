76153b2769edbf07f9eac7790673a5cfbd96e2bf
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-03-28 12:11:35 +0900

    feat: 입력 받는 함수와 입력값을 저장할 데이터 클래스 생성

--- src/main/kotlin/racingcar/Controller.kt
+++ src/main/kotlin/racingcar/Controller.kt
@@ -3,13 +3,10 @@ package racingcar
 import java.util.Random
 import java.util.Scanner
 
+data class RaceInfo(val time : Int, val cars : List<Car>)
+
 fun main() {
-    val input = Scanner(System.`in`)
-    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    val name = input.nextLine().split(',')
-    val cars = name.map { Car(it) }
-    println("시도할 횟수는 몇 회인가요?")
-    val times = input.nextInt()
+    val race : RaceInfo = readInput()
     val random = Random()
 
     repeat(times){
@@ -19,6 +16,17 @@ fun main() {
     val maxPosition = cars.maxOf {it.position}
     val winners = cars.filter { it.position == maxPosition }.map { it.name }
     println("최종 우승자: $winners")
+}
+
+fun readInput() : RaceInfo{
+    val input = Scanner(System.`in`)
+    println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    val names = input.nextLine().split(',')
+    println("시도할 횟수는 몇 회인가요?")
+    val time = input.nextInt()
+
+    val cars = names.map { Car(it) }
+    return RaceInfo(time, cars)
 }
 
 fun randomMove(cars : List<Car>, random: Random) {
