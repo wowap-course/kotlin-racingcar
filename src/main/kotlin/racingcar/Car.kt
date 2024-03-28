9ef3937c90d1634eea3d988305808d5c4655b95b
SeongWooHong <pluto0303@naver.com>
2024-03-28 20:16:08 +0900

    feat: 자동차 이름은 5글자를 넘을 수 없다.

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,12 +1,15 @@
 package racingcar
 
-class Car(private val name: String) {
-    private var distance: Int = INIT_NUMBER
+class Car(val name: String) {
+    var distance: Int = INIT_NUMBER
+
+    init {
+        //if(name.length > 5) throw IllegalArgumentException("")
+        require(name.length > 5){"자동차 이름의 길이는 5를 초과할 수 없습니다."}
+//        check()
+    }
 
     fun moveForward() {
         distance++
     }
-
-    fun getName() = name
-    fun getDistance() = distance
 }
