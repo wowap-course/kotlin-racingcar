7b6494eade67ecedf72d7f2412a4a50b84186ab5
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-03-29 14:32:28 +0900

    [자동차 경주 step1] 김균호 미션 제출합니다. (#5)
    * filename 변경
    
    * feat: Car class 생성
    
    * feat: 사용자로부터 입력받기
    
    * feat: Car 객체 및 toString 생성
    
    * feat: 무작위 수에 따른 차량 이동 함수
    
    * feat: 우승자 출력
    
    * fix: 반복횟수 변경
    
    * refactor: 랜덤 변수, cars 리스트 반복문 변경
    
    * feat: 자동차 이름 길이 예외 처리
    
    * fix: 우승 조건, 출력 형태 변경
    
    * docs: README 작성
    
    * docs: 리뷰에 맞게 README 형식 및 내용 추가
    
    * refactor: Car내부에서 이름 조건 판단, toString 제거
    
    * feat: 입력 받는 함수와 입력값을 저장할 데이터 클래스 생성
    
    * fix: 타입이 변경된 데이터로 인한 오류 수정
    
    * feat: Car 객체의 위치 상태를 출력할 프로퍼티 생성
    
    * fix: Car 출력 프로퍼티 반환타입 수정
    
    * docs: README 완료한 미션 체크

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,14 @@
+package racingcar
+
+class Car(input : String) {
+    val name : String
+    var position : Int = 0
+    init{
+        if(input.length > 5) throw IllegalArgumentException("Name can't be longer than 5")
+        name = input
+    }
+    fun move(condition : Int) {
+        if(condition > 3) position++
+    }
+    fun getPosition() = "-".repeat(position)
+}
