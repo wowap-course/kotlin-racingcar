151ca06bf1a8c141ae43ec12db069b016c15e9c6
김민석|Minseok Kim <alstjr7437@naver.com>
2024-03-29 12:34:11 +0900

    fix: 예외처리 부분 init으로 수정

--- racingcar/racingcar/Car.swift
+++ racingcar/racingcar/Car.swift
@@ -4,22 +4,28 @@
 //
 //  Created by KimMinSeok on 3/26/24.
 //
+enum CarInputError: Error {
+    case invalidArgumentException
+}
 
 import Foundation
 
-class Car{
-    var name:String
+class Car {
+    var name: String
     var position = 0
     
-    init(name: String) {
+    init(name: String) throws {
         self.name = name
+        
+        if !(1...5).contains(name.count) {
+            throw CarInputError.invalidArgumentException
+        }
     }
 
-    func go(){
+    func go() {
         let randomNumber = Int.random(in: 0..<10)
-        if (randomNumber <= 4){
+        if (randomNumber <= 4) {
             position += 1
         }
     }
-    
 }
--- racingcar/racingcar/main.swift
+++ racingcar/racingcar/main.swift
@@ -4,45 +4,34 @@
 //
 //  Created by KimMinSeok on 3/26/24.
 //
-enum carInputError : Error{
-    case invalidArgumentException
-}
+
 
 import Foundation
 
 func main(){
-    guard let cars = inputData() else {
-        return
+    do {
+        let cars = try inputData()
+        
+        print("시도할 횟수는 몇회인가요?")
+        let count = Int(readLine()!)!
+        
+        goRacing(count: count, cars: cars)
+        findWinners(cars: cars)
+    } catch {
+        print("입력한 자동차 이름이 올바르지 않습니다.")
     }
-    
-    print("시도할 횟수는 몇회인가요?")
-    let count = Int(readLine()!)!
-
-    goRacing(count: count, cars: cars)
-    findWinners(cars: cars)
 }
 
-func inputData() -> [Car]?{
-    do {
-        print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-        var cars: [Car] = []
-        let carInput = readLine()!.split(separator: ",").map{ String($0) }
-        
-        for car in carInput{
-            if car.count > 5{
-                throw carInputError.invalidArgumentException
-            }
-        }
-        
-        for name in carInput{
-            cars.append(Car(name: name))
-        }
-        
-        return cars
-    } catch {
-        print("Error: 차 이름이 5글자가 넘었습니다.")
-        return nil
+func inputData() throws -> [Car]{
+    print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+    var cars: [Car] = []
+    let carInput = readLine()!.split(separator: ",").map{ String($0) }
+    
+    for name in carInput{
+        cars.append(try Car(name: name))
     }
+    
+    return cars
 }
 
 
