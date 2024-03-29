122d7cfd2dcfe2df9ef4edb30ae270df27245de5
이정금 <jnggmi@gmail.com>
2024-03-29 19:36:49 +0900

    자동차 생성, 자동차들의 진행상황 내부적 구현

--- untitled1/src/main/kotlin/Main.kt
+++ untitled1/src/main/kotlin/Main.kt
@@ -1,22 +1,52 @@
+import kotlin.random.Random
 fun main(){
-    opengame()
-
-
-}
-
-fun opengame(){
     print("자동차 이름을 입력하세요. 콤마로 구분하여 자동차를 추가합니다")
     val names= readln()
     val namelist=names.split(",")
     print(namelist)
-    for (i in 0..namelist.size){
-        var icar=Car(namelist[i])
-//        Car.name=namelist[i]
-        //Car객체생성;자동차이름=namelist[i]
-
+    val cars = mutableListOf<Car>()
+    for(name in namelist) {
+        cars.add(Car(name))
     }
+    print("몇번 시도할까요?")
+    val num = readln()
+    val rap = num.toInt()
+    for (i in 0..namelist.size-1)
+        for (j in 1..rap) {
+            print("H")
+            //자동차 움직임 구현
+            cars[i].accel()
+            print(cars[i].progres)
+        }
+    for (i in 0..namelist.size-1)
+        print(cars[i].progres)
 }
 
-class Car(name: String ="", progres: Int =0){
-
+class Car(val name: String,var progres: Int){
+    constructor(name: String):this(name,0)
+fun accel(){
+    var nnn=Random.nextInt(1,10)
+    if (nnn>=4) progres++
+ }
 }
+//fun opengame(){
+//    print("자동차 이름을 입력하세요. 콤마로 구분하여 자동차를 추가합니다")
+//    val names= readln()
+//    val namelist=names.split(",")
+//    print(namelist)
+//    val cars = mutableListOf<Car>()
+//     for(name in namelist) {
+//         cars.add(Car(name))
+//     }
+//print("몇번 시도할까요?")
+//val num = readln()
+//val rap = num.toInt()
+//    for (i in 0..namelist.size)
+//     for (j in 1..rap) {
+//       print("hi")
+//       //자동차 움직임 구현
+//       cars[i].accel()
+//        }
+//for (i in 0..namelist.size)
+//    print(cars[i].progres)
+//}
