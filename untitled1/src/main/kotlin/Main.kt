da6779413a7c5a822d9c2fc0a310331ae2b72988
이정금 <jnggmi@gmail.com>
2024-03-29 21:18:39 +0900

    필요 없는 부분(테스트용포함)진행상황을 1번째 차 2번째차--1번째 차 2번쨰 차 순으로 나오게 수정함, 우승자 구현할 것

--- untitled1/src/main/kotlin/Main.kt
+++ untitled1/src/main/kotlin/Main.kt
@@ -1,13 +1,15 @@
 import kotlin.random.Random
 fun main(){
+    //opening
     print("자동차 이름을 입력하세요. 콤마로 구분하여 자동차를 추가합니다")
     val names= readln()
     val namelist=names.split(",")
     print(namelist)
     val cars = mutableListOf<Car>()
-    for(name in namelist) {
-        cars.add(Car(name))
+    for(nam in namelist) {
+        cars.add(Car(nam))
     }
+    //progrss
     print("몇번 시도할까요?")
     val num = readln()
     val rap = num.toInt()
@@ -15,37 +17,32 @@ fun main(){
         for (i in 0..namelist.size-1) {
             cars[i].accel()
         }
-
+    //end
+    var result= mutableListOf(0)
+    var winner= mutableListOf(0)
+    winner.remove(0)
+    for (i in 0..namelist.size-1) {
+         result.add(cars[i].progres)
+    }
+    val winning=result.max()
+    for (i in 0..namelist.size-1) {
+        if(winning == cars[i].progres){
+            val winnerlist= mutableListOf(cars[i].name)
+    println("승자는")
+    for (name in winnerlist )
+            print(name)
+    println("입니다")
+        }
+    }
 }
+
+
 
 class Car(val name: String,var progres: Int){
     constructor(name: String):this(name,0)
 fun accel(){
     var nnn=Random.nextInt(1,10)
     if (nnn>=4) progres++
-    println(
-        "$name:"+ "-".repeat(progres)
-    )
+    println("$name:"+ "-".repeat(progres))
  }
 }
-//fun opengame(){
-//    print("자동차 이름을 입력하세요. 콤마로 구분하여 자동차를 추가합니다")
-//    val names= readln()
-//    val namelist=names.split(",")
-//    print(namelist)
-//    val cars = mutableListOf<Car>()
-//     for(name in namelist) {
-//         cars.add(Car(name))
-//     }
-//print("몇번 시도할까요?")
-//val num = readln()
-//val rap = num.toInt()
-//    for (i in 0..namelist.size)
-//     for (j in 1..rap) {
-//       print("hi")
-//       //자동차 움직임 구현
-//       cars[i].accel()
-//        }
-//for (i in 0..namelist.size)
-//    print(cars[i].progres)
-//}
