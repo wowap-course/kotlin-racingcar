8f94f50d0e7ccdcc867747b2610af43d4fb74bc1
홍성우 <98575563+Redish03@users.noreply.github.com>
2024-03-29 13:54:39 +0900

    [자동차 경주 step1] 홍성우 미션 제출합니다. (#7)
    * chore: initial commit
    
    * feat: add Car class
    
    * feat: add fun moveForward in Car class
    
    * feat: add RacingManager
    
    * docs: add RacingController information
    
    * feat: add print method
    
    * feat: add getter
    
    * feat: add Manager in main
    
    * feat: add gameStart fun
    
    * feat: add fun for game operate
    
    * feat: get car name from user
    
    * feat: add fun printResultCommand
    
    * feat: add fun randoms
    
    * feat: add fun createNewCar
    
    * feat: add fun inputRepeatTime
    
    * refactor: Make Car class name property as private
    
    * feat: add fun printOperateResult
    
    * feat: gameRun
    
    * refactor: fix line & space
    
    * feat: add fun printWinner
    
    * feat: add fun printWinner
    
    * refactor: change lazy to Constructor & var
    
    * refactor: fix printWinner
    
    * refactor: move fun printWinner
    
    * refactor: fix inputCarName
    
    * refactor: fix printOperateResult
    
    * feat: add Printer.kt
    
    * feat: add printer fun
    
    * refactor: change number as macro
    
    * refactor: change number to constant
    
    * feat: If error occured, throw IllegalArgumentException
    
    * refactor: fun name fix
    
    * refactor: convention
    
    * feat: check name exception
    
    * refactor: change winnerCars var to val
    
    * docs: Write README file
    
    * docs: 마크다운 컨벤션 맞추기
    
    * refactor: 코틀린 컨벤션 맞추기
    
    * refactor: 매직넘버 상수화
    
    * feat: 자동차 이름은 5글자를 넘을 수 없다.
    
    * refactor: Car 프로퍼티 접근 지정자 public로 변경
    
    * refactor: 상수 companion object로 변경
    
    * refactor: 상수 companion object로 변경
    
    * refactor: fix Constant
    
    * fix: 부호 반대로
    
    * refactor: 상수화
    
    * refactor: RacingManager가 다른 함수에 호출되지 않도록 하기
    
    * refactor: RacingManager가 다른 함수에 호출되지 않도록 하기
    
    * refactor: 필요 없는 함수 제거
    
    * refactor: 필요 없는 함수 제거
    
    * refactor: make distance setter private
    
    * refactor: change name
    
    * refactor: delete unnecessary property

---
+++ src/main/kotlin/racingcar/RacingManager.kt
@@ -0,0 +1,105 @@
+package racingcar
+
+class RacingManager {
+    private val racingCars = mutableListOf<Car>()
+    private var repeatTime: Int = INIT_NUMBER
+    private val printer = Printer()
+
+    fun run() {
+        gameStart()
+        gameRun()
+        printWinner()
+    }
+
+    private fun gameStart() {
+        printer.printInsertCarNameCommand()
+        inputCarName()
+        printer.printInsertRepeatTimeCommand()
+        inputRepeatTime()
+    }
+
+    private fun gameRun() {
+        printer.printResultCommand()
+        for (i in START_OF_REPEAT..repeatTime) {
+            decideGoOrNot()
+            printRoundResult()
+            println()
+        }
+    }
+
+    private fun printWinner() {
+        printer.printFinalWinnerCommand()
+        val winnerNames = setWinner().joinToString(", ") { it.name }
+        printer.printWinnerName(winnerNames)
+    }
+
+    private fun decideGoOrNot() {
+        for (car in racingCars) {
+            if (randomNumberOverFour()) {
+                car.moveForward()
+            }
+        }
+    }
+
+    private fun randomNumberOverFour(): Boolean = randomNumberCreate() >= VALIDATE_RANDOM_NUMBER
+
+    private fun randomNumberCreate() = (MIN_VALUE_OF_RANDOM_NUMBER..MAX_VALUE_OF_RANDOM_NUMBER).random()
+
+    private fun inputCarName() {
+        try {
+            val inputCarName: List<String> = readln().split(INPUT_SEPARATER)
+            for (name in inputCarName) {
+                checkBlank(name)
+                racingCars.add(Car(name, INIT_NUMBER))
+            }
+        } catch (e: Exception) {
+            throw IllegalArgumentException("잘못된 입력")
+        }
+    }
+
+    private fun checkBlank(name: String) {
+        if (name.isBlank()) {
+            throw Exception()
+        }
+    }
+
+    private fun inputRepeatTime() {
+        try {
+            repeatTime = readln().toInt()
+        } catch (e: Exception) {
+            throw IllegalArgumentException("잘못된 입력")
+        }
+    }
+
+    private fun printRoundResult() {
+        for (car in racingCars) {
+            printer.printCarName(car)
+            repeat(car.distance) {
+                printer.printDash()
+            }
+            println()
+        }
+    }
+
+    private fun setWinner() : MutableList<Car> {
+        val maxDistance = racingCars.maxOfOrNull { it.distance }
+        val winnerCars = mutableListOf<Car>()
+
+        for (car in racingCars) {
+            if (car.distance == maxDistance) {
+                winnerCars.add(car)
+            }
+        }
+
+        return winnerCars
+    }
+
+    companion object {
+        const val INIT_NUMBER = 0
+        const val MIN_VALUE_OF_RANDOM_NUMBER = 0
+        const val MAX_VALUE_OF_RANDOM_NUMBER = 9
+        const val INPUT_SEPARATER = ","
+        const val VALIDATE_RANDOM_NUMBER = 4
+        const val START_OF_REPEAT = 1
+    }
+}
