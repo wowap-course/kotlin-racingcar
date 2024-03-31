a8a82fa4c202ba86315db54bc4ff9d0bc80710e1
SeongWooHong <pluto0303@naver.com>
2024-03-31 11:39:31 +0900

    feat(InputView.kt): add fun inputCarName & inputRepeatTime

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,5 +1,23 @@
 package racingcar.view
 
 class InputView {
+    fun inputCarName(): List<String> {
+        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
+        return readln().split(INPUT_SEPARATER)
+    }
 
+    private fun checkBlank(name: String) {
+        if (name.isBlank()) {
+            throw Exception()
+        }
+    }
+
+    fun inputRepeatTime(): Int {
+        println("시도할 횟수는 몇 회인가요?")
+        return readln().toInt()
+    }
+
+    companion object {
+        const val INPUT_SEPARATER = ","
+    }
 }
