7f4ee3859a864f437c8fc47d896e0900345d2f50
SeongWooHong <pluto0303@naver.com>
2024-03-31 22:17:28 +0900

    refactor(OutputView.kt): delete unnecessary fun

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,14 +3,6 @@ package racingcar.view
 import racingcar.domain.Car
 
 class OutputView {
-    fun printInsertCarNameCommand() {
-        println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
-    }
-
-    fun printInsertRepeatTimeCommand() {
-        println("시도할 횟수는 몇 회인가요?")
-    }
-
     fun printResultCommand() {
         println("\n실행 결과")
     }
