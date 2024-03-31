cf77be1360e2b315e641cd31ddd7ff118085c4a3
SeongWooHong <pluto0303@naver.com>
2024-03-31 12:31:19 +0900

    feat(Car.kt): add fun moveForward and randomNumber generate

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,17 +1,18 @@
 package racingcar.domain
 
+import racingcar.domain.RacingManager.Companion.VALIDATE_RANDOM_NUMBER
+
 class Car(val name: String, initDistance: Int) {
     var distance: Int = initDistance
         private set
 
     init {
-//        if(name.length > 5) throw IllegalArgumentException("")
         require(name.length < MAX_NAME_LENGTH){"자동차 이름의 길이는 5를 초과할 수 없습니다."}
-//        check()
     }
 
     fun moveForward() {
-        distance++
+        val randomNumber = (RacingManager.MIN_VALUE_OF_RANDOM_NUMBER..RacingManager.MAX_VALUE_OF_RANDOM_NUMBER).random()
+        if (randomNumber >= VALIDATE_RANDOM_NUMBER) distance++
     }
 
     companion object {
