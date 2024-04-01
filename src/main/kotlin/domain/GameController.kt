2566e6f8c1fa2f2c226dd2fba115911f706f02ed
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-01 23:54:12 +0900

    feat: 입력된 횟수에 따른 경기 진행

--- src/main/kotlin/domain/GameController.kt
+++ src/main/kotlin/domain/GameController.kt
@@ -1,11 +1,19 @@
 package domain
 
 import view.InputView
+import view.OutputView
 
-class GameController(private val inputView : InputView) {
-    fun run(){
+class GameController(
+    private val inputView: InputView,
+    private val outputView: OutputView,
+) {
+    fun run() {
         val names = inputView.readCarNames()
         val time = inputView.readRaceTime()
-    }
+        val Cars : List<Car> = names.map { Car(it) }
 
+        repeat(time){
+            Cars.forEach { it.move() }
+        }
+    }
 }
