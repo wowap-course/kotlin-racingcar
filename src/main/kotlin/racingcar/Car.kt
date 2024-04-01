7e572945ac48e1574860fcf75904280d6425f475
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-01 23:07:44 +0900

    refactor: require를 사용한 Car name 조건 개선

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,8 +4,7 @@ class Car(val name : String, initPosition : Int = 0) {
     var position : Int = initPosition
         private set
     init{
-        if(input.length > 5) throw IllegalArgumentException("Name can't be longer than 5")
-        name = input
+        require(name.length in 1..5)
     }
     fun move(condition : Int) {
         if(condition > 3) position++
