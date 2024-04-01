991aa371994c5e62e9fa6aea39a49ad9131a9e56
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-01 23:40:39 +0900

    refactor: GameController에서 inputView를 다루는 run 함수 생성

--- src/main/kotlin/domain/GameController.kt
+++ src/main/kotlin/domain/GameController.kt
@@ -3,5 +3,8 @@ package domain
 import view.InputView
 
 class GameController(private val inputView : InputView) {
-
+    fun run(){
+        inputView.readCarNames()
+        inputView.readRaceTimes()
+    }
 }
