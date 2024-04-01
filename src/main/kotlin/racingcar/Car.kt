257da5e6d2b6d49676f89012003defe70205e2ef
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-01 23:22:06 +0900

    rafactor: Car class 코드의 가독성을 위해 숫자를 문자로 변경

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,10 +4,18 @@ class Car(val name : String, initPosition : Int = 0) {
     var position : Int = initPosition
         private set
     init{
-        require(name.length in 1..5)
+        require(name.length in NAME_MAX_LENGTH..NAME_MIN_LENGTH)
     }
     fun move() {
-        val randomNum = (0..9).random()
-        if (randomNum >= 4) position++
+        val randomNum = (RANDOM_MAX_VALUE..RANDOM_MIN_VALUE).random()
+        if (randomNum >= MOVEMENT_CONDITION) position++
+    }
+
+    companion object{
+        private val RANDOM_MAX_VALUE = 0
+        private val RANDOM_MIN_VALUE = 9
+        private val NAME_MAX_LENGTH = 1
+        private val NAME_MIN_LENGTH = 5
+        private val MOVEMENT_CONDITION = 4
     }
 }
