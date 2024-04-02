301c78ce68e9886a935605c898f3fa13281aaec9
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-02 17:35:59 +0900

    bugFix: JudgementTest에서 너무 긴 자동차 이름을 수정

--- src/test/kotlin/racingcar/domain/JudgementTest.kt
+++ src/test/kotlin/racingcar/domain/JudgementTest.kt
@@ -7,11 +7,11 @@ class JudgementTest {
     @Test
     fun `심판은 가장 멀리 있는 자동차의 위치를 파악할 수 있다`(){
         val judgement = Judgement()
-        val car1 = Car("first", 5)
-        val car2 = Car("second", 2)
+        val car1 = Car("win", 5)
+        val car2 = Car("lose", 2)
 
         val winners = judgement.judgeTheWinner(listOf(car1, car2))
 
-        assertThat(winners).containsExactly("first")
+        assertThat(winners).containsExactly("win")
     }
 }
