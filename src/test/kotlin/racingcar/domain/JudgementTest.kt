fb38418af9c3e61eacf86f6f5a756a05680b5244
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-02 17:32:58 +0900

    feat: 심판의 우승자 판단에 대한 Test 추가

--- src/test/kotlin/racingcar/domain/JudgementTest.kt
+++ src/test/kotlin/racingcar/domain/JudgementTest.kt
@@ -1,4 +1,17 @@
 package racingcar.domain
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class JudgementTest {
+    @Test
+    fun `심판은 가장 멀리 있는 자동차의 위치를 파악할 수 있다`(){
+        val judgement = Judgement()
+        val car1 = Car("first", 5)
+        val car2 = Car("second", 2)
+
+        val winners = judgement.judgeTheWinner(listOf(car1, car2))
+
+        assertThat(winners).containsExactly("first")
+    }
 }
