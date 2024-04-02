15f399bde9b725a423f7930b4aac91b651389b38
김민석|Minseok Kim <alstjr7437@naver.com>
2024-04-02 14:09:57 +0900

    test: CarTest 구현 완료

---
+++ racingcar/RacingCarTest/domain/RacingCarTest.swift
@@ -0,0 +1,28 @@
+//
+//  CarTest.swift
+//  racingCarUnitTest
+//
+//  Created by KimMinSeok on 4/2/24.
+//
+
+import XCTest
+@testable import racingcar
+
+
+final class CarTest: XCTestCase {
+    
+//    func test_자동차는_이름을_가짐() throws{
+//        XCTAssertThrowsError(try Car(name: "Testtss"))
+//    }
+    
+    func test_자동차는_이름을_가짐() throws{
+        XCTAssertThrowsError(try Car(name: "Testtss"))
+    }
+
+
+    func test_자동차_움직임_테스트() throws{
+        let testCar = try Car(name:"Test")
+        XCTAssertNoThrow(testCar.go()) // go() 메서드 호출 시 예외가 발생하지 않는지 확인
+        XCTAssertEqual(testCar.position, 1)
+    }
+}
