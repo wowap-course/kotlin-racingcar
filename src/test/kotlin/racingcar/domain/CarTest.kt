c5860a932f8ef4e1126b39393f728baa3180b177
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-04-02 17:23:08 +0900

    feat: 자동차의 이름, 길이에 대한 Test 추가

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -1,4 +1,18 @@
 package racingcar.domain
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 class CarTest {
+    @Test
+    fun `자동차는 이름을 가진다`() {
+        val car = Car("formula")
+        assertThat(car.name).isEqualTo("formula")
+    }
+
+    @Test
+    fun `자동차 이름의 길이는 5를 초과할 수 없다`() {
+        assertThrows<IllegalArgumentException> { Car("abcdef") }
+    }
 }
