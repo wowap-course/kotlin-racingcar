ce02e4c9d8cce7f0d5b88b8188c391bc86765fba
SeongWooHong <pluto0303@naver.com>
2024-04-02 13:35:54 +0900

    refactor(Car.kt): 이름에 공백 포함될 시 IllegalArgumentException 발생

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -6,6 +6,7 @@ class Car(val name: String, initDistance: Int) {
 
     init {
         require(name.length < MAX_NAME_LENGTH) { "자동차 이름의 길이는 5를 초과할 수 없습니다." }
+        require(!name.contains(" ")) { "자동차의 이름에 공백이 포함되어있으면 안됩니다." }
     }
 
     fun moveForward() {
